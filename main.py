68e19cd9c0a15499e4fa94f74ee3a48cd57f09ed
Markus Thiede <mailtothemaru@gmail.com>
2025-10-19 20:37:11 +0200

    implemented report of character counts

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import count_words_in_string, get_num_of_each_unique_characters
+from stats import (
+    count_words_in_string,
+    get_num_of_each_unique_characters,
+    generate_sorted_list_of_characters,
+)
 
 
 def get_book_text(filepath):
@@ -9,11 +13,21 @@ def get_book_text(filepath):
 
 
 def main():
-    frankenstein_content = get_book_text("./books/frankenstein.txt")
+    path = "./books/frankenstein.txt"
+    frankenstein_content = get_book_text(path)
     amount_words = count_words_in_string(frankenstein_content)
     character_counts = get_num_of_each_unique_characters(frankenstein_content)
+    sorted_char_list = generate_sorted_list_of_characters(character_counts)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     print(f"Found {amount_words} total words")
-    print(character_counts)
+    print("--------- Character Count -------")
+    for entry in sorted_char_list:
+        if entry["char"].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,17 @@ def get_num_of_each_unique_characters(text):
         else:
             character_counts[char] = 1
     return character_counts
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def generate_sorted_list_of_characters(character_dict):
+    list_of_characters = []
+    for character, num in character_dict.items():
+        list_of_characters.append({"char": character, "num": num})
+
+    list_of_characters.sort(reverse=True, key=sort_on)
+
+    return list_of_characters
