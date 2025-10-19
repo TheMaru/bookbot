ba36badd889236585b8a97c8a8b8c8d7dce1f0fd
Markus Thiede <mailtothemaru@gmail.com>
2025-10-19 20:05:28 +0200

    implemented character count dict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words_in_string
+from stats import count_words_in_string, get_num_of_each_unique_characters
 
 
 def get_book_text(filepath):
@@ -11,7 +11,9 @@ def get_book_text(filepath):
 def main():
     frankenstein_content = get_book_text("./books/frankenstein.txt")
     amount_words = count_words_in_string(frankenstein_content)
+    character_counts = get_num_of_each_unique_characters(frankenstein_content)
     print(f"Found {amount_words} total words")
+    print(character_counts)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words_in_string(string):
     words = string.split()
     return len(words)
+
+
+def get_num_of_each_unique_characters(text):
+    character_counts = {}
+    for character in text:
+        char = character.lower()
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
+    return character_counts
